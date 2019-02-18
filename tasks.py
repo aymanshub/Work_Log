d4fe034301e061e32bd56be5de6fba952419b7cb
Ayman Said <ayman-said@live.com>
2019-02-18 23:45:30 +0200

    search menu

--- tasks.py
+++ tasks.py
@@ -2,10 +2,11 @@ import datetime
 import csv
 import sys
 
+
 class Task:
 
     def __init__(self, date=None, name=None, duration=0, notes=None):
-        self.date = datetime.datetime.now() if date is None else date
+        self.date = datetime.date.today() if date is None else date
         self.name = name
         self.duration = duration
         self.notes = notes
@@ -20,17 +21,41 @@ class Task:
                                        notes=self.notes)
 
     def write_to_csv(self, filename='work_log.csv'):
-        # verify if file exists, with the right header
-        with open(filename, "a", newline='') as csvfile:
-            fieldnames = ['date', 'task name', 'time spent', 'notes']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({
-                'date': self.date,
-                'task name': self.name,
-                'time spent': self.duration,
-                'notes': self.notes,
-            })
 
+        f_exit = True  # indicates if the program should be terminated due to potential errors on file open\write
+        # verify if file exists, with the right header
+        try:
+            with open(filename, "a", newline='') as csvfile:
+                fieldnames = ['date', 'task name', 'time spent', 'notes']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writerow({
+                    'date': self.date,
+                    'task name': self.name,
+                    'time spent': self.duration,
+                    'notes': self.notes,
+                })
+        except FileNotFoundError:
+            print("The file: {} is not found.".format(filename))
+        except Exception as e:
+            print("Error in input file: {}\n{}".format(filename, e))
+        else:
+            f_exit = False
+
+        if f_exit:
+            exit(1)  # Exits the program due to the caught errors
+
+    @classmethod
+    def load_from_log(cls, filename='work_log.csv'):
+        tasks_Dict = {}
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                new_task = cls(row['date'], row['task name'], row['time spent'], row['notes'])
+                if row['date'] not in tasks_Dict:
+                    tasks_Dict[row['date']] = []
+                tasks_Dict[row['date']].append(new_task)
+
+        return tasks_Dict
 
 
 def _flog_exists(filename='work_log.csv'):
--- work_log.py
+++ work_log.py
@@ -13,8 +13,33 @@ from tasks import Task
 running_program = True
 
 
-def main_menu():
+def display_tasks(tasks):
+    for i, task in enumerate(tasks, start=1):
+        while True:
+            # clear the screen
+            os.system("cls" if os.name == "nt" else "clear")
+            print(task)
+            print("\nResult {} of {}\n".format(i, len(tasks)))
+            # Next, Edit, Delete, Return to search menu
+            if i < len(tasks):
+                print("[N]ext, [E]dit, [D]elete, [R]eturn to search menu")
+            else:
+                print("[E]dit, [D]elete, [R]eturn to search menu")
+            option = input()
 
+            if option.lower() in ['n', 'next'] and i < len(tasks):
+                continue
+            elif option.lower() in ['e', 'edit']:
+                # call edit function
+                break  # back to main menu
+            elif option.lower() in ['d', 'Delete']:
+                # call delete function
+                break  # back to main menu
+            elif option.lower() in ['r', 'return']:
+                pass
+
+
+def main_menu():
     welcome_msg = "Welcome To Work Log"
 
     print("-" * len(welcome_msg))
@@ -31,36 +56,71 @@ def main_menu():
 
 
 def add_new_entry():
-    # set task date
-    date_input = input("Date of the task:\nPlease use DD/MM/YYYY: ")  # get the task date from the user
-    task_date = datetime.datetime.strptime(date_input, '%d/%m/%Y').date()
-    # ToDo validate date correctness
+    while True:
+        # set task date
+        date_input = input("Date of the task:\nPlease use DD/MM/YYYY: ")  # get the task date from the user
+        try:
+            task_date = datetime.datetime.strptime(date_input, '%d/%m/%Y').date()
+        except ValueError:
+            print("Invalid date!!!, try again...")
+            continue
+        else:
+            break
 
-    # set task name
-    task_name = input("Title of the task: ")
-    # ToDo clean trailing white-spaces
+    while True:
+        # set task name
+        task_name = input("Title of the task: ").strip()
+        if not task_name:
+            print("Please enter a meaningful title!")
+            continue
+        else:
+            break
 
-    # set task duration
-    task_duration = int(input("Time spent (rounded minutes): "))
-    # ToDo validate correctness
+    while True:
+        try:
+            # set task duration
+            task_duration = int(input("Time spent (rounded minutes): "))
+        except ValueError:
+            print("Please enter a valid time duration in minutes!")
+            continue
+        else:
+            break
 
     # set task note
-    task_notes = input("Notes (Optional, you can leave this empty): ")
+    task_notes = input("Notes (Optional, you can leave this empty): ").strip()
 
     new_entry = Task(date=task_date,
                      name=task_name,
                      duration=task_duration,
                      notes=task_notes)
 
+    # write entry to CSV
     new_entry.write_to_csv()
     print("The following entry has been added:")
     print(new_entry)
 
-    # writeToCSV
 
+def search_menu():
+    os.system("cls" if os.name == "nt" else "clear")  # clear the screen
+    print("Do you want to search by:")
 
-def search_in_existing_entries():
-    pass
+    search_menu_selection = input("What would you like to do?\n"
+                                  "a) Exact Date\n"
+                                  "b) Range of Dates\n"
+                                  "c) Exact Search\n"
+                                  "d) Regex Pattern\n"
+                                  "e) Return to menu\n")
+    return search_menu_selection
+
+def search_existing_entries():
+    tasks_dict = Task.load_from_log()  # loads all existing tasks in the work log CSV file
+    print("From below dates list\nPlease select a date index:")
+    dates = enumerate(tasks_dict.keys(), start=1)
+    for i, date in dates:
+        print('{}) {}'.format(i, date))
+    index = input()
+    selected_tasks = tasks_dict[list(tasks_dict.keys())[int(index) - 1]]
+    # display_tasks
 
 
 if __name__ == '__main__':
@@ -77,10 +137,11 @@ if __name__ == '__main__':
             break
 
         elif user_selection.upper() in ["ADD", "A"]:
-            # call add new entry function
-            add_new_entry()
+            os.system("cls" if os.name == "nt" else "clear")  # clear the screen
+            add_new_entry()  # call add new entry function
             break
 
         elif user_selection.upper() in ["SEARCH", "B"]:
-            search_in_existing_entries()
-
+            search_menu()
+            # search_existing_entries()  # call search existing entries function
+            break
