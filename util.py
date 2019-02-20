30e1dfee89e33cb606f98c665781e0ef30dc1e0b
Ayman Said <ayman-said@live.com>
2019-02-20 20:37:48 +0200

    starting search by duration

--- util.py
+++ util.py
@@ -4,6 +4,7 @@ from tasks import Task
 
 date_fmt = '%d/%m/%Y'
 
+
 def main_menu():
     while True:
         # clear the screen
@@ -27,36 +28,30 @@ def main_menu():
         elif user_selection.upper() in ["QUIT", "C"]:
             print("Quitting...Hope to see you soon!")
             break
-        else:
-            continue
 
 
 def search_menu():
-    os.system("cls" if os.name == "nt" else "clear")  # clear the screen
-    user_selection = input("Please select your desirable search method:\n"
-                           "a) Select a Date\n"
-                           "b) Range of Dates\n"
-                           "c) Time spent\n"
-                           "d) Exact Search\n"
-                           "e) Regex Pattern\n"
-                           "f) Return to menu\n")
     while True:
+        os.system("cls" if os.name == "nt" else "clear")  # clear the screen
+        user_selection = input("Please select your desirable search method:\n"
+                               "a) Select a Date\n"
+                               "b) Range of Dates\n"
+                               "c) Time spent\n"
+                               "d) Exact Search\n"
+                               "e) Regex Pattern\n"
+                               "f) Return to Main menu\n")
+
         if user_selection.upper() in ["Select a Date".upper(), "A"]:
             search_by_date()  # call search by date function
-            break
         elif user_selection.upper() in ["Range of Dates".upper(), "B"]:
             search_range_of_dates()  # call search by range of dates function
-            break
         elif user_selection.upper() in ["Time spent".upper(), "C"]:
             search_time_spent()  # call search by time spent function
-            break
         elif user_selection.upper() in ["Exact Search".upper(), "D"]:
             search_exact_value()  # call search by exact value function
-            break
         elif user_selection.upper() in ["Regex Pattern".upper(), "E"]:
             search_regex_pattern()  # call search by regex pattern function
-            break
-        elif user_selection.upper() in ["Return to menu".upper(), "F"]:
+        elif user_selection.upper() in ["Return to Main menu".upper(), "F"]:
             break
 
 
@@ -98,77 +93,95 @@ def add_new_entry(for_edit=False):
 
     # create a new task instance
     new_task = Task(date=task_date,
-                     name=task_name,
-                     duration=task_duration,
-                     notes=task_notes)
+                    name=task_name,
+                    duration=task_duration,
+                    notes=task_notes)
 
     # check for duplicates, task should not be more than once in the log.
     tasks_dict = Task.load_from_log()
-    """
-    if new_task.date.strftime(date_fmt) in tasks_dict.keys():
-        if new_task in tasks_dict[new_task.date.strftime(date_fmt)]:
-            print("Task already exists!")
-            """
-    if new_task not in list(tasks_dict.values()):
+    if new_task.date.strftime(date_fmt) in tasks_dict.keys() and \
+            new_task in tasks_dict[new_task.date.strftime(date_fmt)]:
+        if not for_edit:
+            print("Such task already exists!")
+    else:
         # write task to log file
         new_task.add_task_to_file()
-        print("The following task has been successfully added:")
-        print(new_task)
-    else:
-        print("Task already exists!")
-    # if new_task.date.strftime(date_fmt) in tasks_dict.keys():
-    #     if new_task not in tasks_dict[new_task.date.strftime(date_fmt)]: # selected_tasks = tasks_dict[list(tasks_dict.keys())[int(index) - 1]]
-    #         # write task to log file
-    #         new_task.add_task_to_file()
-    #         print("The following task has been successfully added:")
-    #         print(new_task)
-    #     else:
-    #         print("Task already exists!")
+        if for_edit:
+            print("\nTask edit completed successfully.")
+        else:
+            print("\nThe task has been added successfully.")
+
+    input("Press enter to continue...")
 
 
 def display_selected_tasks(tasks):
-    for i, task in enumerate(tasks, start=1):
+    i = 0
+    while True:
         # clear the screen
         os.system("cls" if os.name == "nt" else "clear")
-        print(task)
-        print("\nResult {} of {}\n".format(i, len(tasks)))
-        # Next, Edit, Delete, Return to search menu
-        if i < len(tasks):
-            print("[N]ext, [E]dit, [D]elete, [R]eturn to search menu")
-        else:
-            print("[E]dit, [D]elete, [R]eturn to search menu")
+        print(tasks[i])
+        print("\nResult {} of {}\n".format(i+1, len(tasks)))
+        commands = "[E]dit, [D]elete, [R]eturn to search menu"
+        if i < len(tasks) - 1:
+            # Add Next command
+            commands = "[N]ext, " + commands
+            if i > 0:
+                # Add Back command
+                commands = "[B]ack, " + commands
+        elif i > 0:
+            # Add Back command
+            commands = "[B]ack, " + commands
+        print(commands)
         option = input()
 
-        if option.lower() in ['n', 'next'] and i < len(tasks):
-            continue
+        if option.lower() in ['n', 'next']:
+            if i < len(tasks) - 1:
+                i += 1
+        elif option.lower() in ['b', 'back']:
+            if i > 0:
+                i -= 1
         elif option.lower() in ['e', 'edit']:
             # clear the screen
             os.system("cls" if os.name == "nt" else "clear")
-            print("Please edit the following task:\n{}".format(task))
-            task.delete_task_from_log()
+            print("Please edit the following task:\n{}".format(tasks[i]))
+            tasks[i].delete_task_from_log()
             add_new_entry(for_edit=True)
-            break  # back to main menu
+            break  # back to search menu
         elif option.lower() in ['d', 'Delete']:
             # call delete function
-            task.delete_task_from_log()
-            break  # back to main menu
+            tasks[i].delete_task_from_log()
+            break  # back to search menu
         elif option.lower() in ['r', 'return']:
-            break
+            break  # back to search menu
 
 
 def search_by_date():
     # get the whole exiting tasks from the tasks log file and loads it into a dictionary
-    # clear the screen
-    os.system("cls" if os.name == "nt" else "clear")
-    tasks_dict = Task.load_from_log()  # loads all existing tasks in the work log CSV file
-    print("From below dates list\nPlease select a date index:")
-    dates = enumerate(tasks_dict.keys(), start=1)
-    for i, date in dates:
-        print('{}) {}'.format(i, date))
-    index = input()
-    selected_tasks = tasks_dict[list(tasks_dict.keys())[int(index) - 1]]
-    # selected_tasks = tasks_dict.get(int(index) - 1)
-    display_selected_tasks(selected_tasks)
+    while True:
+        # clear the screen
+        os.system("cls" if os.name == "nt" else "clear")
+        tasks_dict = Task.load_from_log()  # loads all existing tasks in the work log CSV file
+        print("From below dates list\nPlease select a date index:")
+        dates = enumerate(tasks_dict.keys(), start=1)
+        for i, date in dates:
+            print('{}) {}'.format(i, date))
+        max_index = len(tasks_dict)+1
+        print('{}) Return to Search menu'.format(max_index))
+        try:
+            index = int(input())
+            if 1 <= index <= max_index: # range of valid selections
+                if index == max_index:
+                    break # return to Search menu
+                else:
+                    selected_tasks = tasks_dict[list(tasks_dict.keys())[index - 1]]
+            else:
+                raise ValueError
+        except ValueError:
+            input("Invalid selection, please press Enter to try again...")
+            continue
+        else:
+            display_selected_tasks(selected_tasks)
+            return # go back to Search menu
 
 
 def search_range_of_dates():
